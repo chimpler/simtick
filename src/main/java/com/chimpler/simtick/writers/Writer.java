e93481f5c400fd20c021af8645762deb93a2ee2e
Francois Dang Ngoc <francois.dangngoc@gmail.com>
2016-02-21 00:32:53 -0500

    added code coverae

--- src/main/java/com/chimpler/simtick/writers/Writer.java
+++ src/main/java/com/chimpler/simtick/writers/Writer.java
@@ -1,8 +1,6 @@
 package com.chimpler.simtick.writers;
 
 public abstract class Writer<T> {
-    public Writer() {}
-
     public abstract boolean isDelta(T value);
     public abstract int writeRaw(byte[] buffer, T t, int offset);
     public abstract int writeDelta(byte[] buffer, T t, int offset);
